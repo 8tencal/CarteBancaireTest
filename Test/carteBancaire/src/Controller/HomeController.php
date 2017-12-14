7430b1ad8188ab3de2b2efe13925cd058960c5e3
Manu <emmdeleon@hotmail.com>
2017-12-14 18:20:30 +0100

    Ajout du changement de nom

--- Test/carteBancaire/src/Controller/HomeController.php
+++ Test/carteBancaire/src/Controller/HomeController.php
@@ -2,23 +2,42 @@
 
 namespace App\Controller;
 
+use Twig\Environment;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 
+
 class HomeController
 {
 
-	/**
+	
+
+	 private $twig;
+
+    public function __construct(Environment $twig)
+    {
+    	$this->twig = $twig;
+    }
+
+
+
+    /**
 	* @Route("/", name = "home" )
 	*
 	* @param Request $request 	
 	* @return Response
 	*/
 
+
 	public function __invoke(Request $request) : Response 
-	{
-		return new Response('Bitch');
+	{	
+		$name = $request->query->get('name','world');
+
+		return new Response(
+
+	   $this->twig->render('pages/home.html.twig',compact('name'))
+	   );
 	}
 }
