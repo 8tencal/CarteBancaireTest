74a2886a6ae34f1fff0434abdf3d312e891b9d53
kerbi <antoine_delsol@hotmail.fr>
2018-02-25 12:38:14 -0800

    Ajout d'un Logout. Modifications de rendu.

--- Test/carteBancaire/tests/Controller/AccueilControllerTest.php
+++ Test/carteBancaire/tests/Controller/AccueilControllerTest.php
@@ -19,10 +19,6 @@ class AccueilControllerTest extends WebTestCase
     {
         $client = static::createClient();
         $crawler = $client->request('GET','/');
-        //var_dump($crawler);
-        //die();
-        //echo $crawler->html();
-        //$this->assertEquals(200,$client->getResponse()->getStatusCode());
         // On test qu'on se fait bien refusé la page d'accueil puisqu'on est pas connecté.
         $this->assertEquals(301,$client->getResponse()->getStatusCode());
         //$this->assertNotContains('COUCOU',$crawler->filter('h1')->text());
